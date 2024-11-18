75d17140bdc9b311e29a53ecc568a9ebf33d4a0e
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-11-18 22:51:58 +0530

    Display cleaned up data on UI

--- script.js
+++ script.js
@@ -14,22 +14,37 @@ const getFullWeatherData = async (searchTerm) => {
 const displayUI = (searchTerm) => {
   let data = getFullWeatherData(searchTerm);
   let cityName = document.querySelector(".cityName");
-  let weatherDescription = document.querySelector(".weatherDescription");
+  let weatherDescription = document.querySelector(".currentWeatherDescription");
+  let weatherIcon = document.querySelector(".currentWeather > .icon");
+  let weatherFeelsLike = document.querySelector(".currentWeather > .feelsLike");
+  let weatherTempMax = document.querySelector(".currentWeather > .tempMax");
+  let weatherTempMin = document.querySelector(".currentWeather > .tempMin");
+  let weatherHumidity = document.querySelector(".currentWeather > .humidity");
 
   data
     .then((weatherData) => {
       return {
-        address: weatherData.resolvedAddress,
+        address: weatherData.resolvedAddress
+          .split(",")
+          .filter((_, index) => index !== 1)
+          .toString(),
         weatherDescription: weatherData.description,
         icon: weatherData.currentConditions.icon,
         feelslike: weatherData.currentConditions.feelslike,
-        humidity: weatherData.currentConditions.humidity,
+        humidity: Math.round(weatherData.currentConditions.humidity),
         tempmax: weatherData.days[0].tempmax,
         tempmin: weatherData.days[0].tempmin,
       };
     })
     .then((cleanData) => {
       //use data here
+      cityName.textContent = cleanData.address;
+      weatherDescription.textContent = cleanData.weatherDescription;
+      weatherIcon.textContent = cleanData.icon;
+      weatherFeelsLike.textContent = `Temp: ${cleanData.feelslike}`;
+      weatherTempMax.textContent = `Max: ${cleanData.tempmax}`;
+      weatherTempMin.textContent = `Min: ${cleanData.tempmin}`;
+      weatherHumidity.textContent = `Hum: ${cleanData.humidity}%`;
     });
 };
 
