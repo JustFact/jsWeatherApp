3ec09e8fa0c857959fdc52f07cf220f44b3be0b5
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-11-20 21:03:50 +0530

    Generate cards with upcoming weather data

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ let svgTempHigh =
 let svgTempLow =
   '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>thermometer-chevron-down</title><path d="M17.41 12.17L14.24 9L12.83 10.41L17.41 15L22 10.41L20.58 9M10 13V5C10 3.34 8.66 2 7 2S4 3.34 4 5V13C1.79 14.66 1.34 17.79 3 20S7.79 22.66 10 21 12.66 16.21 11 14C10.72 13.62 10.38 13.28 10 13M7 4C7.55 4 8 4.45 8 5V8H6V5C6 4.45 6.45 4 7 4Z" /></svg>';
 
+let svgTempFeelLike =
+  '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>thermometer-check</title><path d="M10 13V5C10 3.34 8.66 2 7 2S4 3.34 4 5V13C1.79 14.66 1.34 17.79 3 20S7.79 22.66 10 21 12.66 16.21 11 14C10.72 13.62 10.38 13.28 10 13M7 4C7.55 4 8 4.45 8 5V8H6V5C6 4.45 6.45 4 7 4M16.25 15.16L13.5 12.16L14.66 11L16.25 12.59L19.84 9L21 10.41L16.25 15.16" /></svg>';
+
 const getWeatherConditionSVG = async (string) => {
   try {
     let response = await fetch(`./SVG/${string}.svg`);
@@ -32,7 +35,35 @@ const getFullWeatherData = async (searchTerm) => {
 };
 
 const setUpcomingWeatherCards = (days) => {
-  //populate cards here
+  const weatherCards = document.querySelector(".section-2 .weatherCards");
+  let dayCards = [];
+  days.forEach((day) => {
+    let dayCard = document.createElement("div");
+    dayCard.classList.add("card");
+
+    let tempHigh = document.createElement("div");
+    tempHigh.classList.add("high");
+    let tempLow = document.createElement("div");
+    tempLow.classList.add("low");
+    let tempFeelslike = document.createElement("div");
+    tempFeelslike.classList.add("feelLike");
+    let datetime = document.createElement("div");
+    datetime.classList.add("datetime");
+    let icon = document.createElement("img");
+    icon.classList.add("weatherCondition");
+
+    tempHigh.innerHTML = svgTempHigh + ` ${day.tempmax}`;
+    tempLow.innerHTML = svgTempLow + ` ${day.tempmin}`;
+    tempFeelslike.innerHTML = svgTempFeelLike + ` ${day.feelslike}`;
+    datetime.textContent = `${day.datetime}`;
+    getWeatherConditionSVG(day.icon).then((res) => {
+      icon.src = res.url;
+    });
+
+    dayCard.append(...[datetime, icon, tempFeelslike, tempHigh, tempLow]);
+    dayCards.push(dayCard);
+  });
+  weatherCards.replaceChildren(...dayCards);
 };
 
 const displayUI = (searchTerm) => {
